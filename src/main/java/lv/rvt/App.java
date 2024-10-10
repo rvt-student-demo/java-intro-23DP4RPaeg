cd130f16aa2d6100ec6b3355dc6483a44927c772
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-10 12:16:20 +0000

    Facorial uzdevums

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,18 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        // Make your Hello World programm!
+    public static void main( String[] args ) {
+    Scanner scanner = new Scanner(System.in);
+    
+    int sk = scanner.nextInt();
+    int faktorials = 1;
+    for (int i = 1;i <= sk; ++i ) {
+    
+        faktorials *= i;
+        System.out.println(faktorials);
     }
+}
 }
