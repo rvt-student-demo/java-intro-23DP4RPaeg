64445acb70576bd6f25a5daea6fe41359ca1e9cc
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-17 11:14:21 +0000

    Divisible by three

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,21 +5,18 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-    Scanner scanner = new Scanner(System.in);
-    
-    System.out.println("How many times?");
+    DivisibleByThreeInRange(2, 40);
 
-    int reiz = Integer.valueOf(scanner.nextLine());
-    int skaits = 0;
-    
-    while (reiz > skaits){
-        printText();
-        skaits ++;
-
+    }
+    public static void DivisibleByThreeInRange(int viens , int divi) {
+      
+    for (int i = viens; i <= divi; i++){
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
         }
     }
+        
 
-public static void printText() {
-    System.out.println("Aziet");
-}
+
 }
