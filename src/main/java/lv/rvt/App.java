428f76d20999f0591124b3ff84fba20a31ff1405
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-25 10:41:19 +0000

    Advanced astrology(Iesakts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,34 +5,25 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-    printstars(3);
-    System.out.println();
-    printsquare(4);
-    System.out.println();
-    printrectangle(17, 3);
-    System.out.println();
     printTriangle(4);
 }
-    public static void printstars(int number) {
+    public static void printstars(int number){
         for (int i = 0; i < number; i++){
             System.out.print("*");
         }
-        System.out.println();
     }
-    public static void printsquare(int size) { 
-       for (int i = 0; i < size; i++) {
-            printstars(size);
+    
+    public static void printSpaces(int number){
+        for (int i = 0; i < number; i++){
+            System.out.println("");
         }
-        System.out.println();
     }
-   public static void printrectangle(int width, int height){
-       for (int i = 0; i < height; i++){
-            printstars(width);
-       }
-    }
+    
     public static void printTriangle(int size){
         for (int i = 0; i <= size; i++){
+            printSpaces(size);
             printstars(i);
         }
     }
+
 }
