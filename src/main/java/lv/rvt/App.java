705f6ccbb49ad2199333044ebe59e4995bd49b8e
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-25 10:20:20 +0000

    Star Sign(Pabeigts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,18 +5,34 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-    DivisibleByThreeInRange(2, 40);
-
+    printstars(3);
+    System.out.println();
+    printsquare(4);
+    System.out.println();
+    printrectangle(17, 3);
+    System.out.println();
+    printTriangle(4);
+}
+    public static void printstars(int number) {
+        for (int i = 0; i < number; i++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
-    public static void DivisibleByThreeInRange(int viens , int divi) {
-      
-    for (int i = viens; i <= divi; i++){
-            if (i % 3 == 0) {
-                System.out.println(i);
-            }
+    public static void printsquare(int size) { 
+       for (int i = 0; i < size; i++) {
+            printstars(size);
+        }
+        System.out.println();
+    }
+   public static void printrectangle(int width, int height){
+       for (int i = 0; i < height; i++){
+            printstars(width);
+       }
+    }
+    public static void printTriangle(int size){
+        for (int i = 0; i <= size; i++){
+            printstars(i);
         }
     }
-        
-
-
 }
