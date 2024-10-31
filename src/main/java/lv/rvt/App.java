cfd9ab53a2d0a1f7c48bfe9c3f4a1f54148d9c72
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-31 12:45:36 +0000

    Exercise 2 — Two Arrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,11 +6,20 @@ public class App
 {
     public static void main( String[] args ) {
     
-        int[] val = {0, 1, 2, 3};
+        int[] val = {13, -4, 82, 17};
 
-        int sum = val[0] + val[1] + val[2] + val[3];
+        int[] twice = new int[4];
+    
+        twice[0] = val[0];
+        twice[1] = val[1];
+        twice[2] = val[2];
+        twice[3] = val[3];
 
-        System.out.println("Sum of all numbers = " + sum );
+    System.out.println( "Original Array: " 
+        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+
+    System.out.println( "New Array: " 
+        + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
     
     }
 
