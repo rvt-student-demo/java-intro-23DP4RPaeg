aa5346c103a23c3230e40c8ff7ddf0cbaf7ad4ad
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-31 12:48:11 +0000

    Exercise 2 — Two Arrays(Fixed)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,10 +10,10 @@ public class App
 
         int[] twice = new int[4];
     
-        twice[0] = val[0];
-        twice[1] = val[1];
-        twice[2] = val[2];
-        twice[3] = val[3];
+        twice[0] = val[0] * 2;
+        twice[1] = val[1] * 2;
+        twice[2] = val[2] * 2;
+        twice[3] = val[3] * 2;
 
     System.out.println( "Original Array: " 
         + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
