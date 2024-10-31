555aefbc3f476cf2e5a9f8fd84bb7c7b14dd20dc
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-10-31 13:08:54 +0000

    Exercise 2 — Two Arrays(Pareizais pildisanas veids)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,21 +6,21 @@ public class App
 {
     public static void main( String[] args ) {
     
-        int[] val = {13, -4, 82, 17};
+        int[] val = {13, -4, 82, 17}; 
+        int[] twice;
+        twice = new int[4];
 
-        int[] twice = new int[4];
-    
-        twice[0] = val[0] * 2;
-        twice[1] = val[1] * 2;
-        twice[2] = val[2] * 2;
-        twice[3] = val[3] * 2;
+        for (int i = 0; i < 4; i++) {
+            twice[i] = val[i] * 2;
+        }
+
 
     System.out.println( "Original Array: " 
         + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-
+ 
+ 
     System.out.println( "New Array: " 
         + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
-    
     }
 
 }
