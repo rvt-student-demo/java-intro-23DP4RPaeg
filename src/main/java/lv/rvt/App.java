699e92a9a6c8301972195a97272e2fe787bd61a0
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-11-01 12:11:41 +0000

    Personal details(Nepabeigts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-    
-        int[] val = {0, 1, 2, 3}; 
-        int temp;
-
-        for (int i = 0; i < val.lenght/2; i++){
-            temp = val[i];
-            val[i] = val[val.lenght - i - 1];
-            val[val.lenght - i -1] = temp;
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        while (true) {
+            String input = reader.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] parts = input.split(",");
+            sum = sum + Integer.valueOf(parts[1]);
+            count = count + 1;
+        
         }
-
-
-        System.out.println( "Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-     
-     
-        System.out.println( "Reversed Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+        if (count > 0){
+            System.out.println("Average of the birth years: " + (1.0 * sum / count));
+        }
     }
-
 }
