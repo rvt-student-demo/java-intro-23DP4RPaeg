4151f2aeb0d655c63602dd3ebfbaec0b0470ef90
23DP4RPaeg <167166366+23DP4RPaeg@users.noreply.github.com>
2024-11-15 11:32:56 +0000

    Statistics Part 3

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,12 +6,17 @@ public class App
 {
     public static void main( String[] args ) {
         Statistics statistics = new Statistics();
-        statistics.addNumber(3);
-        statistics.addNumber(5);
-        statistics.addNumber(1);
-        statistics.addNumber(2);
-        System.out.println("Count: " + statistics.getCount());
-        System.out.println("Sum: " + statistics.sum());
-        System.out.println("Average: " + statistics.average());
+
+        Scanner reader = new Scanner(System.in);
+        
+        while (true) {
+            int number = reader.nextInt();
+            if (number == -1) {
+                break;
+            }
+            statistics.addNumber(number);
+        }
+
+        System.out.println("Sum: "+statistics.sum());
     }
  }
